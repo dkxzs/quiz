c8f6bba5835dcfb12a8153a03e606d11ae782ac3
Ha Nguyen <hanguyen2505003@gmail.com>
2024-11-09 23:37:56 +0700

    create header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
+import Header from './components/Header/Header';
 
 function App() {
   return (
     <div className="app-container">
       <header className="App-header">
-        hello world
+        <Header />
       </header>
     </div>
   );
