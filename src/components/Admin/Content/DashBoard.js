54326307b401336008fbdfe1384e32747c012399
Ha Nguyen <hanguyen2505003@gmail.com>
2025-01-18 23:04:01 +0700

    design chart

--- src/components/Admin/Content/DashBoard.js
+++ src/components/Admin/Content/DashBoard.js
@@ -1,5 +1,78 @@
+import {
+  Bar,
+  BarChart,
+  CartesianGrid,
+  Legend,
+  XAxis,
+  YAxis,
+  Tooltip,
+} from "recharts";
+import "./DashBoard.scss";
+
 const DashBoard = (props) => {
-  return <div>dashboard</div>;
+  const data = [
+    {
+      name: "Page A",
+      uv: 4000,
+      pv: 2400,
+      amt: 2400,
+    },
+    {
+      name: "Page B",
+      uv: 3000,
+      pv: 1398,
+      amt: 2210,
+    },
+    {
+      name: "Page C",
+      uv: 2000,
+      pv: 9800,
+      amt: 2290,
+    },
+    {
+      name: "Page D",
+      uv: 2780,
+      pv: 3908,
+      amt: 2000,
+    },
+    {
+      name: "Page E",
+      uv: 1890,
+      pv: 4800,
+      amt: 2181,
+    },
+    {
+      name: "Page F",
+      uv: 2390,
+      pv: 3800,
+      amt: 2500,
+    },
+  ];
+
+  return (
+    <div className="dashboard-container">
+      <div className="title">dashboard</div>
+      <div className="content">
+        <div className="left">
+          <div className="child">Total users</div>
+          <div className="child">Total quizzes</div>
+          <div className="child">Total questions</div>
+          <div className="child">Total answers</div>
+        </div>
+        <div className="right">
+          <BarChart width={500} height={300} data={data}>
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis dataKey="name" />
+            <YAxis />
+            <Tooltip />
+            <Legend />
+            <Bar dataKey="pv" fill="#8884d8" />
+            <Bar dataKey="uv" fill="#82ca9d" />
+          </BarChart>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default DashBoard;
